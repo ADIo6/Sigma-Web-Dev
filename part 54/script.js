2a545aa3704bc22a703bde4c3d80b8aa62c7ebf8
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-03-26 23:24:13 +0530

    javascript

---
+++ part 54/script.js
@@ -0,0 +1,19 @@
+alert("hello world");
+
+console.log("Hey code is running")
+console.log("Hey code is running 2")
+console.log("Hey code is running 3")
+var a = prompt("Enter Your Number")
+var isTrue = confirm("Are you sure to leave this page...")
+if(isTrue){
+    console.log("you are being redirected")
+
+} 
+else{
+    console.log("COMPUTER IS IDEL")
+
+}
+console.log("Your number is " + a)
+
+document.title = "This is altered titile"
+document.body.style.backgroundColor= "red"
---
+++ part 55/script.js
@@ -0,0 +1,56 @@
+console.log("hey this is part 55 ")
+
+// var a=5;
+// a+=1;
+// var b=6;
+// var c="adil";
+//var 55a="adil2"; not allowed
+
+let a=5;
+a+=1;
+let b=6;
+let c="adil";
+{
+    let a= 88;
+    console.log(a)
+
+}
+console.log(a)
+
+// console.log(a+b+8+c)
+
+// console.log(typeof a, typeof b, typeof c);
+
+const a1 = 6; 
+
+// a1+=4; // Not allowed because a1 is constant.
+
+let x= "adil ahmed";
+let y=22;
+let z=3.55;
+const p= true;
+let q =undefined;
+let r = null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o={
+    name:"harry",
+    "job code": "0627"
+    ,"is handsome": true
+}
+console.log(o)
+
+o.salary = "200 crores"
+console.log(o)
+o.salary = "500 crores"
+console.log(o)
+
+let student_details ={
+    name:"adil ahmed",
+    "phone number": "9889889800",
+    marks: "89%",
+
+}
+
+console.log(student_details)
