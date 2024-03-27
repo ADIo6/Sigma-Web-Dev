7f1b2779207c5f6a07533b3af2dd736bca1439c9
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-03-28 03:19:12 +0530

    ...

---
+++ part 56/index.js
@@ -0,0 +1,27 @@
+console.log("Hello i am conditional tutorial")
+
+let age= 3;
+
+let grace = 2;
+
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age ** grace)
+if((age+grace)>18)
+{
+    console.log("You can drive")
+
+
+}
+else if(age==0)
+
+{
+    console.log("Are you kidding")
+}
+else
+{
+    console.log("You can not drive")
+
+}
