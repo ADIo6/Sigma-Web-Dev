f23828a7edab7152505de12f31c27546eb22c736
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-03-29 08:07:41 +0530

    strings

---
+++ part 60/index.js
@@ -0,0 +1,32 @@
+console.log("this is strings tutorial")
+let a = "adil"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+// console.log(a[4]);
+
+console.log(a.length)
+
+let rname = "adil"
+let friend = "tanzim"
+// console.log("his name is " +rname+ " and his friend name is " +friend)
+console.log(`his name is "${rname}" and his " friend's " name is ${friend}`)
+ let b = "tunatu"
+ console.log(b.toLocaleLowerCase())
+ console.log(b.toLocaleUpperCase())
+ console.log(b.length)
+ console.log(b.slice(0,3))
+ console.log(b.slice(1))
+
+ console.log(b.replace("tu", "99"))
+
+ console.log(b.concat( a," ahmed"," adi"))
+ console.log(b.endsWith("u"))
+ console.log(b.endsWith("r"))
+
+
+
+
+
+
