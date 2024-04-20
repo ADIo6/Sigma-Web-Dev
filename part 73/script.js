26c1471201e6b93e7c63db202cae54632b210642
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-04-20 23:21:37 +0530

    EXCERSIZE 13
    making a card

---
+++ part 73/script.js
@@ -0,0 +1,5 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail){
+
+}
+
+createCard("Introduction to backend | Sigma web dev video #2","CodeWithAdil", 567000, 7 , "31:21","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ" )
