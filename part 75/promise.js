5c9583dc90bcfb7dac5196a177a5b8bd1044019c
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-04-24 20:28:20 +0530

    callbacks, setTimeout, promises

---
+++ part 75/promise.js
@@ -0,0 +1,98 @@
+console.log('This is prommises');
+
+let prom1= new Promise((resolve, reject)=>{
+
+    let a= Math.random();
+    if(a<0.5){
+        reject("No random number was not supporting you")
+    }
+    else{
+
+        setTimeout(() => {
+            console.log("Yes i am done")
+            resolve("adil")
+        }, 3000);
+        }
+    })
+
+let prom2= new Promise((resolve, reject)=>{
+
+    let a= Math.random();
+    if(a<0.5){
+        reject("No random number was not supporting yo  u 2")
+    }
+    else{
+
+        setTimeout(() => {
+            console.log("Yes i am done 2")
+            resolve("adil2")
+        }, 1000);
+        }
+    })
+let prom3= new Promise((resolve, reject)=>{
+
+    let a= Math.random();
+    if(a<0.5){
+        reject("No random number was not supporting yo  u 3")
+    }
+    else{
+
+        setTimeout(() => {
+            console.log("Yes i am done 3")
+            resolve("adil3")
+        }, 1000);
+        }
+    })
+let prom4= new Promise((resolve, reject)=>{
+
+    let a= Math.random();
+    if(a<0.5){
+        reject("No random number was not supporting you 4")
+    }
+    else{
+
+        setTimeout(() => {
+            console.log("Yes i am done 4")
+            resolve("adil4")
+        }, 1000);
+        }
+    })
+
+
+
+// prom1.then((a)=>{
+//     console.log(a)
+
+// }).catch((err)=>{
+//     console.log(err);
+    
+// })
+
+// let p3= Promise.allSettled([prom1,prom2])
+
+// p3.then((a)=>{
+//         console.log(a)
+    
+//     }).catch((err)=>{
+//         console.log(err);
+        
+//     })
+
+// let p3= Promise.race([prom1,prom2])
+
+// p3.then((a)=>{
+//         console.log(a)
+    
+//     }).catch((err)=>{
+//         console.log(err);
+        
+//     })
+let p3= Promise.any([prom1,prom2,prom3,prom4])
+
+p3.then((a)=>{
+        console.log(a)
+    
+    }).catch((err)=>{
+        console.log(err);
+        
+    })
---
+++ part 75/script.js
@@ -0,0 +1,32 @@
+console.log("adil is a hacker")
+console.log("adil is a heecker")
+
+setTimeout(() => {
+    console.log("i am inside set-timeout")
+
+}, 0);
+
+console.log("The End    ")
+
+const callback = (arg) => {
+    console.log(arg)
+
+  
+}
+
+
+const loadScript = (src, callback) => {
+  let sc= document.createElement("script")
+
+  sc.src=src;
+  sc.onload= callback("Adil")
+  document.head.append(sc)
+
+  
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback)
+
+
+alert(`hello adil`);
+clo
