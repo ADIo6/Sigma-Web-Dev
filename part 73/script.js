55a6c00b9182a2d1e6a19da3e67c2bf82ccfe1c0
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-04-27 19:54:50 +0530

    creating card

--- part 73/script.js
+++ part 73/script.js
@@ -1,5 +1,35 @@
 function createCard(title, cName, views, monthsOld, duration, thumbnail){
+    let viewStr
+    if(views<1000)
+    {
+         viewStr= views
 
+    }
+    else if(views>1000000)
+    {
+         viewStr= views/1000000+ "M"
+
+    }
+    else{
+        viewStr=views/1000+ "K"
+    }
+    let html=`<div class="container">
+    <div class="card">
+        <div class="thumbnail">
+
+            <img src=${thumbnail}>
+
+            <p class="duration">${duration} </p>
+        </div>
+        <div class="text">
+            <h2>${title}</h2>
+            <p>${cName} .  ${viewStr} views. ${monthsOld} months ago </p>
+        </div>
+
+    </div>
+</div>`
+
+document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
 
 createCard("Introduction to backend | Sigma web dev video #2","CodeWithAdil", 567000, 7 , "31:21","https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ" )
