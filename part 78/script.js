4c42c79b21bc2a32cbc6c0186b82ef162e350b46
Adil Ahmed <103695245+ADIo6@users.noreply.github.com>
2024-04-28 21:13:27 +0530

    Excersize 14

---
+++ part 78/script.js
@@ -0,0 +1,19 @@
+
+function hack(){
+    
+}
+let html= ` <div class="container">
+<h2>Initialing Hacking...</h2>
+<h2>Reading your Files...</h2>
+<h2>Password files Detected...</h2>
+<h2>Sending all passwords and personal files to...</h2>
+<h2>Cleaning up...</h2>       
+</div>`
+
+
+let rand=Math.random()
+
+if(rand>0.2)
+{
+    return document.querySelector("h2[0]")
+}
